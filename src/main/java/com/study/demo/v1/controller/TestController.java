622b160a88f047b64b8bf17200b4d2eb13883e51
MIN_GEUN <min0903@adcapsule.co.kr>
2020-06-29 17:38:07 +0900

    서버포트 변경. 테스트 컨트롤러 작성.

---
+++ src/main/java/com/study/demo/v1/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.study.demo.v1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+}
