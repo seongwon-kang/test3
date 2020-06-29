04406480fc5108edce34e6f6572355ba0e58bba6
MIN_GEUN <min0903@adcapsule.co.kr>
2020-06-29 17:57:15 +0900

    TEST 객체 기초 단계 생성 db테스트는 아직.

--- src/main/java/com/study/demo/v1/controller/TestController.java
+++ src/main/java/com/study/demo/v1/controller/TestController.java
@@ -1,16 +1,21 @@
 package com.study.demo.v1.controller;
 
+import com.study.demo.v1.service.TestService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/v1")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final TestService testService;
 
     @GetMapping("/test")
     public String test(){
-        return "test";
+        return "fLayout:/test/test";
     }
 
 }
---
+++ src/main/java/com/study/demo/v1/service/TestService.java
@@ -0,0 +1,7 @@
+package com.study.demo.v1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+}
