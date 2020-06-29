d58d709c36373b67c8822fa4d6edd5e6b6a25b0b
MIN_GEUN <min0903@adcapsule.co.kr>
2020-06-29 17:45:21 +0900

    스프링부트에서 jsp를 이용하기 위한 의존성 추가 및 Config 클래스 생성.

---
+++ src/main/java/com/study/demo/v1/config/TilesConfig.java
@@ -0,0 +1,25 @@
+package com.study.demo.v1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+@Configuration
+public class TilesConfig implements WebMvcConfigurer {
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer configurer = new TilesConfigurer();
+        configurer.setDefinitions(new String[]{"/WEB-INF/tiles/tiles-defs.xml"});
+        configurer.setCheckRefresh(true);
+        return configurer;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        registry.viewResolver(viewResolver);
+    }
+}
